f0e99912e75176ae5f89a240620ac01941d82b66
sai02-creator <saichaitanyagaddam1@gmail.com>
2025-01-04 01:43:16 +1100

    Add products on sale

--- store/models.py
+++ store/models.py
@@ -3,10 +3,16 @@ import datetime
 
 # Categories of Products
 class Category(models.Model):
-    name = models.CharField(max_length=50)
+	name = models.CharField(max_length=50)
 
-    def __str__(self):
-        return self.name
+	def __str__(self):
+		return self.name
+
+	#@daverobb2011
+	class Meta:
+		verbose_name_plural = 'categories'
+
+
 
 
 # Customers
@@ -20,17 +26,21 @@ class Customer(models.Model):
     def __str__(self): 
         return f'{self.first_name} {self.last_name}'
 
-
 # All of our Products
 class Product(models.Model):
-    name = models.CharField(max_length=100)
-    price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
-    description = models.CharField(max_length=250, default='', blank=True, null=True)
-    image = models.ImageField(upload_to='uploads/product/')
+	name = models.CharField(max_length=100)
+	price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+	category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+	description = models.CharField(max_length=250, default='', blank=True, null=True)
+	image = models.ImageField(upload_to='uploads/product/')
+	# Add Sale Stuff
+	is_sale = models.BooleanField(default=False)
+	sale_price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
 
-    def __str__(self):
-        return self.name
+	def __str__(self):
+		return self.name
+
+
 
 
 # Orders
