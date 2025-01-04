ec1c5787810d45a36fec896de2655777d9504ac9
sai02-creator <saichaitanyagaddam1@gmail.com>
2025-01-04 23:56:39 +1100

    Define about

--- store/views.py
+++ store/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from .models import Product
 
 def home(request):
-    return render(request, 'home.html', {})
+    products = Product.objects.all()
+    return render(request, 'home.html', {'products':products})
+
+def about(request):
+	return render(request, 'about.html', {})	
