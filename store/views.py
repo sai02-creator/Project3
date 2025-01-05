3312ce40845b778c8658ebc812311669aa4e7729
sai02-creator <saichaitanyagaddam1@gmail.com>
2025-01-05 23:55:18 +1100

    Add register_user

--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
     products = Product.objects.all()
@@ -9,9 +10,28 @@ def home(request):
 def about(request):
 	return render(request, 'about.html', {})
 
-
 def login_user(request):
-     return render(request, 'login.html', {})
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:  # Check if authentication was successful
+            messages.success(request, "You Have Been Logged In!")
+            return redirect('home')
+        else:
+            messages.error(request, "There was an error, please try again...")
+            return redirect('login')
+    else:
+      return render(request, 'login.html', {})
+
+
+
 
 def logout_user(request):
-      pass
+      logout(request)
+      messages.success(request, ("You have been logged out...Thanks for stopping by"))
+      return redirect('home')
+
+def register_user(request):
+     return render(request, 'register.html', {})
