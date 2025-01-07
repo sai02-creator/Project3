ef682743812bd1a60fa05f5678d0173d0e6dbdba
sai02-creator <saichaitanyagaddam1@gmail.com>
2025-01-08 00:31:14 +1100

    Retrieve cart products in cart_summary for display

--- cart/views.py
+++ cart/views.py
@@ -9,27 +9,34 @@ from django.http import JsonResponse
 
 
 def cart_summary(request):
-    return render(request, "cart_summary.html", {})
-
-def cart_add(request):
     # Get the cart 
     cart = Cart(request)
-    # test for POST
-    if request.POST.get('action') == 'post':
-        # Get stuff 
-        product_id = int(request.POST.get('product_id'))
-        # lookup product in DB
-        product = get_object_or_404(Product, id=product_id)
-        # Save to session
-        cart.add(product=product) 
-        # Get Cart Quantity 
-        cart_quantity = cart.__len__()
+    cart_products = cart.get_prods
+    return render(request, "cart_summary.html", {"cart_products":cart_products})
 
+def cart_add(request):
+	# Get the cart
+	cart = Cart(request)
+	# test for POST
+	if request.POST.get('action') == 'post':
+		# Get stuff
+		product_id = int(request.POST.get('product_id'))
+		
 
-        # Return response 
-        # response = JsonResponse({'Product Name: ': product.name})
-        response = JsonResponse({'qty': cart_quantity})
-        return response
+		# lookup product in DB
+		product = get_object_or_404(Product, id=product_id)
+		
+		# Save to session
+		cart.add(product=product)
+
+		# Get Cart Quantity
+		cart_quantity = cart.__len__()
+
+		# Return resonse
+		# response = JsonResponse({'Product Name: ': product.name})
+		response = JsonResponse({'qty': cart_quantity})
+    
+		return response
 
 
 
