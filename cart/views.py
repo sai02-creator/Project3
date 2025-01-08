c94094250aa9853040ce2669f4d0a765d0acc152
sai02-creator <saichaitanyagaddam1@gmail.com>
2025-01-09 00:05:19 +1100

    Retrieve quantities of products from the cart

--- cart/views.py
+++ cart/views.py
@@ -12,7 +12,8 @@ def cart_summary(request):
     # Get the cart 
     cart = Cart(request)
     cart_products = cart.get_prods
-    return render(request, "cart_summary.html", {"cart_products":cart_products})
+    quantities =  cart.get_quants 
+    return render(request, "cart_summary.html", {"cart_products":cart_products, "quantities":quantities})
 
 def cart_add(request):
 	# Get the cart
