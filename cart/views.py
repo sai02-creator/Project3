598c852576d2cc4d7d0e6bef191440382e1e747b
sai02-creator <saichaitanyagaddam1@gmail.com>
2025-01-10 01:44:42 +1100

    Add cart total calculation to cart summary view

--- cart/views.py
+++ cart/views.py
@@ -4,16 +4,19 @@ from store.models import Product
 from django.http import JsonResponse
 
 
-
-
-
-
 def cart_summary(request):
-    # Get the cart 
-    cart = Cart(request)
-    cart_products = cart.get_prods
-    quantities =  cart.get_quants 
-    return render(request, "cart_summary.html", {"cart_products":cart_products, "quantities":quantities})
+	# Get the cart
+	cart = Cart(request)
+	cart_products = cart.get_prods
+	quantities = cart.get_quants
+	totals = cart.cart_total()
+	return render(request, "cart_summary.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals})
+
+
+
+
+
+
 
 def cart_add(request):
     # Get the cart
