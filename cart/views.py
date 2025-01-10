2de911b0d47445e7234d858d21da3c2664ee81af
sai02-creator <saichaitanyagaddam1@gmail.com>
2025-01-11 00:04:47 +1100

    Import messages framework for user notifications

--- cart/views.py
+++ cart/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .cart import Cart 
 from store.models import Product
 from django.http import JsonResponse
+from django.contrib import messages
 
 
 def cart_summary(request):
