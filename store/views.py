d8bc03dc1946fdfd9203f51c27981bb80cba670b
sai02-creator <saichaitanyagaddam1@gmail.com>
2025-01-11 01:05:39 +1100

    Import SignUpForm and UpdateUserForm in views

--- store/views.py
+++ store/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .forms import SignUpForm
+from .forms import SignUpForm, UpdateUserForm
 from django import forms
 
 
