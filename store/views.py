343a42e895e7859a595492030ed3a78e1c83a2ad
sai02-creator <saichaitanyagaddam1@gmail.com>
2025-01-14 01:50:10 +1100

    Enhance search functionality to filter by name and description

--- store/views.py
+++ store/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm, UserInfoForm
 from django import forms
+from django.db.models import Q
 
 
 
@@ -15,7 +16,7 @@ def search(request):
 	if request.method == "POST":
 		searched = request.POST['searched']
 		# Query The Products DB Model
-		searched = Product.objects.filter(name__icontains=searched) 
+		searched = Product.objects.filter(Q(name__icontains=searched)| Q(description__icontains=searched)) 
 		# Test for null
 		if not searched:
 			messages.success(request, "That Product Does Not Exist...Please try Again.")
