c63f996aeccc3d5f6b6f2cb9be0cd44e3efd3d22
sai02-creator <saichaitanyagaddam1@gmail.com>
2025-01-14 23:52:27 +1100

    Handle logged-in user and save cart data to profile

--- cart/cart.py
+++ cart/cart.py
@@ -1,4 +1,4 @@
-from store.models import Product 
+from store.models import Product, Profile
 
 class Cart():
 
@@ -33,6 +33,17 @@ class Cart():
 			self.cart[product_id] = int(product_qty)
 
 		self.session.modified = True
+
+		# Deal with logged in user 
+		if self.request.user.is_authenticated:
+			# Get the current user profile 
+			current_user = Profile.objects.filter(user_id=self.request.user.id)
+			# Convert {'3':1, '2':4} to {"3":1, "2":4}
+			carty = str(self.cart)
+			carty = carty.replace("\'", "\"")
+			# Save carty to the Profile Model
+			current_user.update(old_cart=str(carty))
+
 
 	def cart_total(self):
 		# Get product IDS
