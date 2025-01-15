f52ad9bfd3f5b7de52f0b1f50bad8b747bd0490c
sai02-creator <saichaitanyagaddam1@gmail.com>
2025-01-16 01:00:30 +1100

    Add `checkout` view to render checkout page.

--- payment/views.py
+++ payment/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
+
+def checkout(request):
+    return render(request, "payment/checkout.html", {})
+
+
+
 def payment_success(request):
     return render(request, "payment/payment_success.html", {})
