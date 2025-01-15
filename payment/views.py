cdb95fdf13400357c6777ba34beb3cdc0b0308b0
sai02-creator <saichaitanyagaddam1@gmail.com>
2025-01-16 01:32:04 +1100

    Add cart data to `checkout` view for rendering products, quantities, and totals.

--- payment/views.py
+++ payment/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
+from cart.cart import Cart 
 
 
 def checkout(request):
-    return render(request, "payment/checkout.html", {})
+    # Get the cart
+	cart = Cart(request)
+	cart_products = cart.get_prods
+	quantities = cart.get_quants
+	totals = cart.cart_total()
+	return render(request, "payment/checkout.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals})
+
+
+
+
+ 
 
 
 
