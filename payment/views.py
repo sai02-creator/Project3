df30faad5bac8613e3fc89f0339c57f2df9a3d27
sai02-creator <saichaitanyagaddam1@gmail.com>
2025-01-17 00:42:24 +1100

    Added billing info logic for authenticated and guest users

--- payment/views.py
+++ payment/views.py
@@ -1,17 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from cart.cart import Cart 
 from payment.forms import ShippingForm
 from payment.models import ShippingAddress
-
+from django.contrib import messages
 
 def billing_info(request):
-    # Get the cart
-    cart = Cart(request)
-    cart_products = cart.get_prods
-    quantities = cart.get_quants
-    totals = cart.cart_total()
-    shipping_form = request.POST
-    return render(request, "payment/billing_info.html", {"cart_products": cart_products, "quantities": quantities, "totals": totals,"shipping_form":shipping_form})
+    if request.POST:
+        # Get the cart
+        cart = Cart(request)
+        cart_products = cart.get_prods
+        quantities = cart.get_quants
+        totals = cart.cart_total()
+
+        # Check to see if user is logged in
+        if request.user.is_authenticated:
+            return render(request, "payment/billing_info.html", {"cart_products": cart_products, "quantities": quantities, "totals": totals, "shipping_form": request.POST})
+        else:
+            # Not logged in 
+            pass
+
+        shipping_form = request.POST
+        return render(request, "payment/billing_info.html", {"cart_products": cart_products, "quantities": quantities, "totals": totals, "shipping_form": shipping_form})
+    else:
+        messages.success(request, "Access Denied")
+        return redirect('home')
+
+		  
 
 
 def checkout(request):
