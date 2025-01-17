5216035a04ba0ebaabff89a34ed0887c7c308c31
sai02-creator <saichaitanyagaddam1@gmail.com>
2025-01-18 01:35:28 +1100

    Implement cart deletion after order placement

--- payment/views.py
+++ payment/views.py
@@ -58,12 +58,12 @@ def process_order(request):
                         # Create order item
                         create_order_item = OrderItem(order_id=order_id, product_id=product_id, user=user, quantity=value, price=price)
                         create_order_item.save()
-
+            # Delete our cart
+            for key in list(request.session.keys()):
+                if key == "session_key":
+                    # Delete the key
+                    del request.session[key]
             
-
-
-
-
             messages.success(request, "Order Placed!")
             return redirect('home')
 
@@ -94,6 +94,13 @@ def process_order(request):
                         # Create order item
                         create_order_item = OrderItem(order_id=order_id, product_id=product_id, quantity=value, price=price)
                         create_order_item.save()
+            
+            # Delete our cart
+            for key in list(request.session.keys()):
+                if key == "session_key":
+                    # Delete the key
+                    del request.session[key]
+
 
 
 
