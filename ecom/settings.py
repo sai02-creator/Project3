0a4c1182ca41960e14a561adfb473815798cce9f
sai02-creator <saichaitanyagaddam1@gmail.com>
2025-01-20 23:49:59 +1100

    Implement load_dotenv to load environment variables into the project

--- ecom/settings.py
+++ ecom/settings.py
@@ -6,6 +6,10 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Load our environmental variables
+load_dotenv()
+
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
