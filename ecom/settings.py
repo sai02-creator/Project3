a1d810f0cfa8006d704fe658a0a605f811eeb99c
sai02-creator <saichaitanyagaddam1@gmail.com>
2025-01-20 23:47:21 +1100

    Configure PostgreSQL database settings in Django project

--- ecom/settings.py
+++ ecom/settings.py
@@ -68,9 +68,15 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+   'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'postgres.railway.internal',
+        'PORT': '5432',
     }
 }
 
